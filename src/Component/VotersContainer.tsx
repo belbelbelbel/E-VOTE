665796d8c9b7719652d1a103b3b1de29839f5592
belbelbelbel <gronaldchia@gmail.com>
2024-08-22 05:47:04 +0100

    effect on nexthub responsiveness

--- src/Component/VotersContainer.tsx
+++ src/Component/VotersContainer.tsx
@@ -1,10 +1,12 @@
-import { SideBar } from './SideBar'
-import { Outlet, useLocation } from 'react-router'
+import { SideBar } from './SideBar';
+import { Outlet, useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
+// Custom hook to determine if the screen is mobile-sized
 const useIsMobile = () => {
     const [isMobile, setIsMobile] = useState(window.innerWidth < 1280);
+
     useEffect(() => {
         const handleResize = () => {
             setIsMobile(window.innerWidth < 1280);
@@ -19,7 +21,6 @@ const useIsMobile = () => {
 
 export const VotersContainer = () => {
     const token = localStorage.getItem('token');
-    console.log(token);
     const [showOutlet, setShowOutlet] = useState(false);
     const location = useLocation();
     const isMobile = useIsMobile();
@@ -30,10 +31,10 @@ export const VotersContainer = () => {
         localStorage.removeItem("GroupBId");
         localStorage.removeItem("GroupCId");
         window.location.href = "/";
-    }
+    };
 
     useEffect(() => {
-        const handleEletion = async (id: any) => {
+        const handleElection = async (id:any) => {
             try {
                 const res = await fetch(`https://foursquarevgc-election-api.onrender.com/elections/${id}`, {
                     method: 'GET',
@@ -41,36 +42,32 @@ export const VotersContainer = () => {
                         'Content-Type': 'application/json',
                         'Authorization': `Bearer ${token}`
                     }
-                })
+                });
                 const result = await res.json();
-                console.log(result)
+                console.log(result);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
-        }
-        handleEletion(token);
-    }, [token])
+        };
 
+        handleElection(token);
+    }, [token]);
 
     useEffect(() => {
-        if (location.pathname !== "/votes") {
-            setShowOutlet(true);
-        } else {
-            setShowOutlet(false);
-        }
+        setShowOutlet(location.pathname !== "/votes");
     }, [location]);
 
-    const capitalizeFirstLetter = (string: any) => {
+    const capitalizeFirstLetter = (string:any) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
 
-    var name = localStorage.getItem('username');
-    const username = capitalizeFirstLetter(name);
+    const username = capitalizeFirstLetter(localStorage.getItem('username'));
+    const isGroupRoute = ["/votes/GroupA", "/votes/GroupB", "/votes/GroupC"].includes(location.pathname);
 
     return (
         <div className='w-screen relative z-40 h-[100dvh]'>
             {/* Desktop Layout */}
-            <div className='w-screen h-screen flex xl:flex  hidden '>
+            <div className='w-screen h-screen flex xl:flex hidden'>
                 <div className='w-screen h-screen xl:flex hidden flex'>
                     <div className='xl:w-[22%] w-full sidebarbackground h-screen bg-white'>
                         <SideBar />
@@ -83,43 +80,51 @@ export const VotersContainer = () => {
 
             {/* Mobile Layout */}
             <div className='h-full w-full xl:hidden'>
-                <div className='flex md:pt-[4.5rem] nesthub-full pt-[3rem] md:gap-[3rem] gap-[2rem] flex-col'>
-                    <div className='flex items-center h-full justify-center nexthub_1 md:gap-[7vw] gap-[15vw] flex-col'>
-                        <div><img src="/asset/Images/weblogo.png" alt="mOA" className='xl:w-[14vw] w-[46vw] md:w-[44vw] relative md:-toxp-[15vw] xl:top-0 xl:relative' /></div>
-                        <div className='text-[#9C9C9C] xl:text-[43px] text-[5.5vw] md:text-[5vw] fonts-small font-black flex gap-2'>
-                            <span className='text-[#0250FC] fonts-mid'>Welcome,</span>
-                            <span>{username}</span>
+                {!isGroupRoute && (
+                    <div className='flex md:pt-[4.5rem] nesthub-full pt-[3rem] md:gap-[3rem] gap-[2rem] flex-col'>
+                       <div className='flex items-center h-full justify-center nexthub_1 md:gap-[7vw] gap-[15vw] flex-col'>
+                            <div>
+                                <img
+                                    src="/asset/Images/weblogo.png"
+                                    alt="mOA"
+                                    className='xl:w-[14vw] w-[46vw] md:w-[44vw] asolute md:-top-[15vw] xl:top-0 xl:relative'
+                                />
+                            </div>
+                            <div className='text-[#9C9C9C] xl:text-[43px] text-[5.5vw] md:text-[5vw] fonts-small font-black flex gap-2'>
+                                <span className='text-[#0250FC] fonts-mid'>Welcome,</span>
+                                <span>{username}</span>
+                            </div>
+                        </div>
+                        <div className='w-[85%] group_but md:w-[74%] pt-[1rem] mx-auto flex flex-col md:gap-4 gap-2'>
+                            <div className='fonts-mid text-[4.3vw]'>Select a category to vote</div>
+                            <div className='flex flex-col md:gap-10 gap-5'>
+                                <Link to="GroupA" className='bg-[#0250FC] group_button fonts-small font-extrabold rounded-[10px] flex items-center text-white text-[4.7vw] tracking-[4px] justify-center w-[100%] md:h-[9vh] mx-auto h-[8vh]'>
+                                    <button>GROUP A</button>
+                                </Link>
+                                <Link to="GroupB" className='bg-[#0250FC] group_button fonts-small font-extrabold rounded-[10px] flex items-center text-white text-[4.7vw] tracking-[4px] justify-center w-[100%] md:h-[9vh] mx-auto h-[8vh]'>
+                                    <button>GROUP B</button>
+                                </Link>
+                                <Link to="GroupC" className='bg-[#0250FC] group_button rounded-[10px] fonts-small font-extrabold flex items-center text-white text-[4.7vw] tracking-[4px] justify-center w-[100%] md:h-[9vh] mx-auto h-[8vh]'>
+                                    <button>GROUP C</button>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className='items-center justify-center flex'>
+                            <div className='bg-[#F94040] cursor-pointer rounded-[6px] group_buttonlogout absolute bottom-8 text-white md:h-[7.6vh] h-[6vh] text-[4.1vw] rounded-[10px] items-center justify-center flex w-[50%] mx-auto'>
+                                <button onClick={handleLogout}>Log Out</button>
+                            </div>
                         </div>
                     </div>
-                    <div className='w-[85%] group_but md:w-[74%] pt-[1rem] mx-auto   flex flex-col md:gap-4 gap-2'>
-                        <div className='fonts-mid text-[4.3vw]'>Select a category to vote</div>
-                        <div className='flex flex-col md:gap-10  gap-5'>
-                            <Link to="GroupA" className='bg-[#0250FC] group_button fonts-small font-extrabold rounded-[10px] flex items-center text-white text-[4.7vw] tracking-[4px] justify-center w-[100%] md:h-[9vh] mx-auto h-[8vh]'>
-                                <button>GROUP A</button>
-                            </Link>
-                            <Link to="GroupB" className='bg-[#0250FC] group_button fonts-small font-extrabold rounded-[10px] flex items-center text-white text-[4.7vw] tracking-[4px] justify-center w-[100%] md:h-[9vh] mx-auto h-[8vh]'>
-                                <button>GROUP B</button>
-                            </Link>
-                            <Link to="GroupC" className='bg-[#0250FC] group_button rounded-[10px] fonts-small font-extrabold flex items-center text-white text-[4.7vw] tracking-[4px] justify-center w-[100%] md:h-[9vh] mx-auto h-[8vh]'>
-                                <button>GROUP C</button>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className='items-center justify-center  flex '>
-                        <div className='bg-[#F94040]  cursor-pointer rounded-[6px] group_buttonlogout absolute bottom-8  text-white cursor-pointer md:h-[7.6vh] h-[6vh] text-[4.1vw] rounded-[10px] items-center justify-center flex w-[50%] mx-auto'>
-                            <button onClick={handleLogout}>Log Out</button>
-                        </div>
-                    </div>
-                </div>
+                )}
 
                 {/* Conditionally render Outlet on mobile */}
                 <div className={`absolute w-full z-50 inset-0 bg-white h-full ${showOutlet ? 'slide-in' : 'hidden'}`}>
-                    {!isMobile || location.pathname !== "/votes" ? <Outlet /> : null}
+                    {(!isMobile || isGroupRoute) && <Outlet />}
                 </div>
             </div>
+
             <div className='flex items-center line_suf justify-center'>
-                <div className="h-[1vw] mx-[1.5vw] md:hidden block w-[40%] z-10  relative  -top-2 md:-top-4 mx-auto rounded-[4px] bg-black">
-                </div>
+                <div className="h-[1vw] mx-[1.5vw] md:hidden block w-[40%] z-10 relative -top-2 md:-top-4 mx-auto rounded-[4px] bg-black"></div>
             </div>
         </div>
     );
