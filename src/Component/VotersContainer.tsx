41e5564aefb656cb7a4a5048904ec58ecc71cd63
belbelbelbel <gronaldchia@gmail.com>
2024-08-23 17:22:45 +0100

    the end of E-VOTING

--- src/Component/VotersContainer.tsx
+++ src/Component/VotersContainer.tsx
@@ -114,7 +114,7 @@ export const VotersContainer = () => {
         const stopTime = new Date(stop_time);
 
         if (now < startTime) {
-            setShowElectionHold(false);
+            setShowElectionHold(true);
             setShowElectionResult(false);
         } else if (now >= stopTime) {
             setShowElectionResult(true);
