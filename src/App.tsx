ca46f18faf27762aa298191ded08bf8a56d80408
KAYODE <caresskakka@gmail.com>
2024-08-23 09:32:23 +0100

    log token.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import { Signin } from "./Component/Auth/Signin";
-import { VotersContainer } from "./Component/VotersContainer";
-import { GroupA } from "./Component/GroupA";
-import { GroupB } from "./Component/GroupB";
-import { GroupC } from "./Component/GroupC";
-import { Greeting } from "./Component/Greeting";
-import "./App.css";
+import './App.css'
+import { Route, Routes, Navigate} from 'react-router-dom'
+import { Signin } from './Component/Auth/Signin'
+import { VotersContainer } from './Component/VotersContainer'
+import { GroupA } from './Component/GroupA'
+import { GroupB } from './Component/GroupB'
+import { GroupC } from './Component/GroupC'
+import { Greeting } from './Component/Greeting'
 
 function App() {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token')
+  console.log(token)
 
   return (
     <div>
+      
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="/votes" element={<VotersContainer />}>
           <Route index element={<Greeting />} />
-          <Route path="GroupA" element={<GroupA />} />
-          <Route path="GroupB" element={<GroupB />} />
-          <Route path="GroupC" element={<GroupC />} />
+          <Route path='GroupA' element={<GroupA />} />
+          <Route path='GroupB' element={<GroupB />} />
+          <Route path='GroupC' element={<GroupC />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
