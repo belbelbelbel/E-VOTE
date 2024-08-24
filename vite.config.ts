a30b8c3dd84318b506c70a836c0117c5867ec70e
belbelbelbel <gronaldchia@gmail.com>
2024-08-24 10:00:26 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": process.env,
-   VITE_API_ENDPOINT: process.env.VITE_API_ENDPOINT,
+    VITE_API_ENDPOINT: JSON.stringify(process.env.VITE_API_ENDPOINT),
   },
 })
