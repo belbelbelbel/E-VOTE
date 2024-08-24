94ee7be419f7f19a0f4e65cb82b282316aba5d4a
KAYODE <caresskakka@gmail.com>
2024-08-24 09:46:27 +0100

    proccess env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+   VITE_API_ENDPOINT: process.env.VITE_API_ENDPOINT,
+  },
 })
